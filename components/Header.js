77ec0a2af230e5899a93e89299eaa5ffa6a36990
Betul Turkes <bbetulturkess@gmail.com>
2023-08-24 00:03:44 +0300

    Denemeler yapıldı

--- components/Header.js
+++ components/Header.js
@@ -2,18 +2,33 @@ import Link from 'next/link';
 import styled from "styled-components";
 
 const HeaderDiv = styled.div`
-color: #f00;
 border: 10px solid #f00;
 padding: 30px;
+display:flex;
+gap:20px;
+& > span{
+    font-size: 30px;
+}
+
 ;`
 
-
+const StyledLink = styled(Link)`
+color: #fff;
+text-decoration: none;
+border : ${({border}) =>`${border}px`} solid yellow;
+font-size: ${({big}) =>(big ? "20px":"12px")};
+&:hover{
+    text-decoration: underline;
+}`
+;
 
 
 export default () => (
     <HeaderDiv>
-        <Link href="/">Ana Sayfa</Link>
-        <Link href="/about">Hakkımızda</Link>
+        <StyledLink href="/" border={20}>Ana Sayfa</StyledLink>
+        <StyledLink href="/about" big>Hakkımızda</StyledLink>
+
+        <span>Ek Açıklamalar</span>
     </HeaderDiv>
     
 
